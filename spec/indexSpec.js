21a87842a197b28e93bb667ec487ed62fb2a8c76
Daniel Bidulock <daniel_bidulock@sil.org>
2020-12-15 11:40:04 -0700

    Clean up

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,7 +1,6 @@
 const Browser = require('zombie');
 const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001; 
 Browser.localhost('example.com', PORT);
-const fs = require('fs');
 const app = require('../app');
 
 describe('agentIndexSpec', () => {
@@ -9,35 +8,12 @@ describe('agentIndexSpec', () => {
 
   beforeEach(done => {
     browser = new Browser({ waitDuration: '30s', loadCss: false });
-//    //browser.debug();
-//    fixtures.load(__dirname + '/../fixtures/agents.js', models.mongoose, function(err) {
-//      models.Agent.findOne({ email: 'daniel@example.com' }).then(function(results) {
-//        agent = results;
-//        models.Agent.findOne({ email: 'lanny@example.com' }).then(function(results) {
-//          lanny = results; 
-//          browser.visit('/', function(err) {
-//            if (err) return done.fail(err);
-//            browser.assert.success();
-            done();
-//          });
-//        }).catch(function(error) {
-//          done.fail(error);
-//        });
-//      }).catch(function(error) {
-//        done.fail(error);
-//      });
-//    });
+    done();
   });
 
-//  afterEach(function(done) {
-//    models.mongoose.connection.db.dropDatabase().then(function(err, result) {
-//      done();
-//    }).catch(function(err) {
-//      done.fail(err);
-//    });
-//  });
-
-  describe('authenticated', () => {
-
+  describe('interface', () => {
+    it('shows a Subscribe button', done => {
+      done.fail();
+    });
   });
 });
