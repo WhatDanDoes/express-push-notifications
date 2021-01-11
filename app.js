b038a7faf7655d44c1f919b4049f9ba735267d90
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-11 16:37:30 -0700

    Recycle users route. Hello, subscribe route

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv');
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const subscribeRouter = require('./routes/subscribe');
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/subscribe', subscribeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
