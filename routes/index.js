bed7038500a71fd1d836cb4a2940de16ae8f1285
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-12 16:04:52 -0700

    Set up to receive flash messages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'express-push-notifications' });
+  res.render('index', { title: 'express-push-notifications', messages: req.flash() });
 });
 
 module.exports = router;
