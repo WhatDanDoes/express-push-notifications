894a3020ef6ce45168f6f0848c7339fe78bfb9d2
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-13 13:00:37 -0700

    Name spec file as per jest requirements

---
+++ spec/index.spec.js
@@ -0,0 +1,24 @@
+//const Browser = require('zombie');
+const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001; 
+//Browser.localhost('example.com', PORT);
+const app = require('../app');
+
+describe('index', () => {
+  let browser;
+
+//  beforeEach(done => {
+//    browser = new Browser({ waitDuration: '30s', loadCss: false });
+//
+//    browser.visit('/', err => {
+//      if (err) return done.fail(err);
+//      done();
+//    });
+//  });
+
+//  describe('interface', () => {
+//    it('shows a Subscribe button', done => {
+//      browser.assert.element('#subscribe-button');
+//      done();
+//    });
+//  });
+});
