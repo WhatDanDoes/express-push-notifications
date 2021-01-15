6f1be3736874c217bb5bfb946109e1b080dcfe6b
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-15 09:14:54 -0700

    Get workable jest-puppeteer first config with useful test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,19 +1,9 @@
 const PORT = process.env.NODE_ENV === 'production' ? 3000 : 3001;
-const app = require('../app');
 
 describe('index', () => {
 
-  afterAll(done => {
-    // 2021-1-13
-    //
-    // https://stackoverflow.com/a/14516195/1356582
-    //
-    // This needs to be closed, otherwise you get a `Jest did not exit one second after the test run has completed` error
-    app.close(done);
-  });
-
   beforeEach(async () => {
-    await page.goto('http://localhost:3001');
+    await page.goto(`http://localhost:${PORT}`);
   });
 
   describe('interface', () => {
