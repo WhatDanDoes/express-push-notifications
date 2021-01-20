3b93f820e14dff5de4aa7537dda44cdc8563637d
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-19 17:00:33 -0700

    Get more in-progress push notification mocking experiments

--- spec/subscribe.spec.js
+++ spec/subscribe.spec.js
@@ -292,7 +292,6 @@ describe('subscribe', () => {
           // Method 2.
           //
           //const dialog = await expect(page).toDisplayDialog(async () => {
-          //
           //  console.log("Inside DIALOG expect block")
           //  await expect(page).toClick('#subscribe-button', { text: 'Subscribe' });
           //});
@@ -309,34 +308,118 @@ describe('subscribe', () => {
          * OS notification system
          */
         it.only('receives a successfully-subscribed push message', async done => {
+//          let session;
+//          browser.on('targetcreated', async (target) => {
+//            console.log('Target created');
+//            page = await target.page();
+//            await initializeCDP(page);
+//            session = await page.target().createCDPSession();
+//          });
 
-          const rest = require('msw').rest;
-          const setupServer = require('msw/node').setupServer;
-          const server = setupServer(
-            // Describe the requests to mock.
-            rest.get('https://fcm.googleapis.com/0:1611003913926410%7031b2e6f9fd7ecd', (req, res, ctx) => {
-              endpointHit = true;
-              console.log("ENDPOINT HIT");
-              console.log(res);
+          let session = await page.target().createCDPSession();
+          console.log('CDP session');
+          console.log(session);
 
-              return res(
-                ctx.status(201),
-                ctx.json({
-                  message: 'What happens here, from the mws?',
-                })
-              );
-            }),
-          );
-          server.listen();
+          /**
+           * 2021-1-19 https://jsoverson.medium.com/using-chrome-devtools-protocol-with-puppeteer-737a1300bac0
+           *
+           * This isn't particularly useful, though it does show the CDP stuff
+           * does actually work (run tests in development mode to see the event
+           * fire).
+           *
+           * Now how to make it work for notifications...
+           */
+          await session.send('Network.enable');
+          await session.send('Network.setRequestInterception', { patterns: [
+            {
+              urlPattern: '*',
+              resourceType: 'Script',
+              interceptionStage: 'HeadersReceived'
+            }
+          ]});
 
+          session.on('Network.requestIntercepted', ({ interceptionId }) => {
+            console.log("NETWORK CONNECTION INTERCEPTED");
+            session.send('Network.continueInterceptedRequest', {
+              interceptionId,
+            });
+          });
 
+          /**
+           * This is the stuff I'm interested in...
+           */
+          // Finally got service workers to update. Seperate into new test
+          await session.send('ServiceWorker.enable');
+          await session.send('ServiceWorker.stopAllWorkers');
+          await session.send('ServiceWorker.setForceUpdateOnPageLoad', {forceUpdateOnPageLoad: true});
+          session.on('ServiceWorker.workerRegistrationUpdated', async reg => {
+            console.log("The service worker was updated");
+            console.log(reg);
+            await session.send('ServiceWorker.startWorker', { scopeURL: reg.registrations[0].scopeURL });
+//            let res  = await session.send('ServiceWorker.inspectWorker', { versionId: reg.registrations[0].registrationId });
+//            console.log(res);
 
-          browser.on('dialog', dialog => {
-            console.log("DIALOG IN VIEW");
+            const dialog = await expect(page).toDisplayDialog(async () => {
+              console.log("Inside DIALOG expect block");
+              //await expect(page).toClick('#subscribe-button', { text: 'Subscribe' });
+
+              // Try triggering the push event
+              try {
+                let res = await session.send('ServiceWorker.deliverPushMessage', {
+                  registrationId: reg.registrations[0].registrationId,
+                  origin: reg.registrations[0].scopeURL,
+                  data: JSON.stringify({ message: "Word." })
+                });
+                console.log('Push triggered');
+                console.log(res);
+
+              }
+              catch (err) {
+                console.error(err);
+              };
+
+            });
+
+            console.log('dialog');
+            console.log(dialog);
+
+          });
+
+//          session.on('ServiceWorker.workerErrorReported', err => {
+//            console.log("** There was a ServiceWorker error");
+//            console.log(err.errorMessage);
+//          });
+//
+//          await session.send('BackgroundService.clearEvents', {service: 'notifications'});
+//          await session.send('BackgroundService.setRecording', {shouldRecord: true, service: 'notifications'});
+////          await session.send('BackgroundService.startObserving', {service: 'notifications'});
+
+          session.on('BackgroundService.backgroundServiceEventReceived', dialog => {
+            console.log("IS THIS THE SERVICE WORKER???");
             done();
           });
 
+          // Push notification uses OS's messaging
+          // This will never fire
+          session.on('dialog', dialog => {
+            console.log('DIALOG EVENT FIRED');
+            done();
+          });
+
+          await page.goto(APP_URL, {
+            waintUntil: 'networkidle2',
+            timeout: 10000
+          });
+
+          //
+          // What is best practices when registering service workers?
+          //
+          // Apart from permissions, should it be automatic registration, or should
+          // an action be taken in the app?
+          //
           await expect(page).toClick('#subscribe-button', { text: 'Subscribe' });
+
+//          await session.send('BackgroundService.clearEvents', {service: 'notifications'});
         });
       });
     });
