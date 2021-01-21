a3993eaec84190a2c741996d7e1c4d65d1eb3759
Daniel Bidulock <daniel_bidulock@sil.org>
2021-01-21 15:21:01 -0700

    Introduce service-worker _wrapper_ middleware

--- app.js
+++ app.js
@@ -42,6 +42,60 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+/**
+ * This prepares service workers for inspection with `jest-puppeteer`
+ *
+ * The whole intent is to wrap the `worker.js` functionality so that
+ * console.errors can be thrown. This is how `jest-puppeteer` finds
+ * something _catchable_.
+ */
+if (process.env.NODE_ENV === 'test') {
+  const fs = require('fs');
+
+  console.log('Testing...');
+  console.log(process.env.NODE_ENV);
+
+  app.use('/worker.js', function(req, res, next) {
+    console.log('Mocking ServiceWorker endpoint');
+
+    //
+    // This is what happens normally
+    //
+    //return res.sendFile(`${__dirname}/public/worker.js`);
+
+    //
+    // This is what happens now..
+    //
+    // The most useful, testable thing is the function signature passing
+    // through the `push` event. Beyond this, not much has presented itself as
+    // _testable_ with regard to Push Notifications.
+    //
+    fs.readFile(`${__dirname}/public/worker.js`, 'utf8', (err, data) => {
+      if (err) return done.fail(err);
+
+      //
+      // I have discovered that a ServiceWorker does not trigger the same
+      // events on `console.log` as in-page Javascript. So far, the only way I
+      // can determine what is happening inside my service worker is to output
+      // `console.errors`, which trigger a `ServiceWorker.workerErrorReported`,
+      // which can be _heard_ by `jest-puppeteer`.
+      //
+      data = data.replace(/console\.log/g, 'console.error');
+      res.set({
+        'Content-Type': 'application/javascript; charset=UTF-8',
+        'Content-Length': data.length,
+      });
+
+      // 2021-1-21 https://stackoverflow.com/questions/38788721/how-do-i-stream-response-in-express
+      res.write(data);
+    });
+  });
+}
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
